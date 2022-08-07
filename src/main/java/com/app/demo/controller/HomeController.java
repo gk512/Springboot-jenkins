222699823049bd82a271c25143cb5ccd2b2dabcc
gk512 <gopikrishna.gajula1234@outlook.com>
2022-08-07 14:15:59 +0530

    second commit

--- src/main/java/com/app/demo/controller/HomeController.java
+++ src/main/java/com/app/demo/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.app.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
+	public static Logger log=LoggerFactory.getLogger(HomeController.class);
+	
 	
 	@GetMapping("/home")
 	public String welcome() {
+		log.info("Entered to Main method....");
 		return "welcome to spring boot project with jenkins ";
 	}
 
--- src/test/java/com/app/demo/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/app/demo/SpringbootJenkinsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.app.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootJenkinsApplicationTests {
 
+	public static Logger log=LoggerFactory.getLogger(SpringbootJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		log.info("Entered into Test package");
 	}
 
 }
